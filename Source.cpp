68750b7a08515895e2b03590314bd3a9315a395a
unknown <phamduykhanh30052004@gmail.com>
2025-11-04 11:11:36 +0700

    DuyKhanh_ktrasohoanhao

--- Source.cpp
+++ Source.cpp
@@ -9,6 +9,15 @@ int hieu(int a, int b){
 }
 int tich(int a, int b);
 float thuong(int a, int b);
+bool isPerfect(int n){
+	if(n<=1) return false;
+	int sum=0;
+	for(int i=1;i<n;i++){
+		if(n%i==0){
+			sum+=i;
+		}
+	}
+}
 int main()
 {
 	int a,b;
@@ -19,6 +28,13 @@ int main()
 	cin>> b;
 	cout<<"Tong =" tong(a+b) "\n";
 	cout<<"Hieu =" hieu(a-b)"\n";
+	int n;
+	cout<<"Nhap so n: "
+	cin>>n;
+	if(isPerfect(n))
+	cout<<n<<"La so hoan hao.\n";
+	else
+	cout<<n<<"Khong la so hoan hao.\n";
 	system("pause");
 	return 0;
 }
